c5a5a20264f4f6f52a510e4ce8fd6ad359372d15
ruslan.sharipov <ruslan.sharipov@devexpress.com>
2019-05-30 01:00:54 +0300

    inital

---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+opkg install dnscrypt-proxy fake-hwclock
+
+echo "no-resolv" > /jffs/configs/dnsmasq.conf.add
+echo "server=127.0.0.1#65053" >> /jffs/configs/dnsmasq.conf.add
+
+domains=$(cat domains.txt | tr '\n' '\')
+unblock="unblock"
+echo "$domains$unblock" >> /jffs/configs/dnsmasq.conf.add
+
+echo "/opt/etc/init.d/S09dnscrypt-proxy start" >> /jffs/scripts/services-start
+
+cp firewall-start /jffs/scripts/ -f
+chmod +x /jffs/scripts/firewall-start
+
+cp wan-start /jffs/scripts/ -f
+chmod +x /jffs/scripts/wan-start
+
+cp openvpn-event /jffs/scripts/ -f
+chmod +x /jffs/scripts/openvpn-event
